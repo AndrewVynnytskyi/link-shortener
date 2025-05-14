bb46affa83a0bb3cd1ce1598d03578ad6a268f7b
AndrewVynnytskyi <146383822+AndrewVynnytskyi@users.noreply.github.com>
2025-05-14 20:09:13 +0300

    refactor: improve code formatting and add dotenv configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
+import {UrlModule} from "./urls/url.module";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+if (!DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined');
+}
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot(DATABASE_URL), UrlModule],
 })
-export class AppModule {}
+export class AppModule {
+}
